9279c3472254f3dddbca3265493732f800c5732a
Slava Zubrin <szubrin79@gmail.com>
2024-01-06 18:57:24 +0200

    Day 4, part 2

--- advent2023.playground/Pages/day 4.xcplaygroundpage/Contents.swift
+++ advent2023.playground/Pages/day 4.xcplaygroundpage/Contents.swift
@@ -3,9 +3,11 @@
 import Foundation
 
 struct Card {
+    typealias ID = Int
+
+    let id: ID
     let myWinNumbersCount: Int
 
-    private let id: Int
     private let winningNumbers: Set<Int>
     private let myNumbers: Set<Int>
 
@@ -49,7 +51,44 @@ let inputData = try Data(contentsOf: inputUrl!)
 let input = String(data: inputData, encoding: .utf8)!.components(separatedBy: "\n")
 
 let cards = input.compactMap({ Card(string: $0) })
-
 let result1 = cards.map({ $0.winPower }).reduce(0, +)
+print("result1: \(result1)")
+
+var result2: Int = 0
+var cache = [Card.ID: Int]()
+
+let addCardAndProceed: (Card) -> (Int) = { card in
+    if let cachedCount = cache[card.id] {
+        result2 += cachedCount
+        return cachedCount
+    }
+
+    var localCount = 1
+
+    defer {
+        cache[card.id] = localCount
+    }
+
+    guard card.myWinNumbersCount > 0 else {
+        result2 += 1
+        return localCount
+    }
+
+    let lowerCardIdx = min(cards.count - 1, card.id)
+    let upperCardIdx = min(cards.count - 1, card.id + card.myWinNumbersCount - 1)
+
+    for idx in lowerCardIdx ... upperCardIdx {
+        guard cards.indices.contains(idx) else { break }
+        localCount += addCardAndProceed(cards[idx])
+    }
+
+    result2 += 1
+
+    return localCount
+}
+
+cards.forEach { _ = addCardAndProceed($0) }
+
+print("result2: \(result2)")
 
 //: [Next](@next)
